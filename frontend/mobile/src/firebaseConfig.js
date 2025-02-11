0311ef3cb751293c3c6b406a35564b79d74e705d
ishaanJ91 <jaini@tcd.ie>
2025-02-11 19:00:41 +0000

    added user info

--- frontend/mobile/src/firebaseConfig.js
+++ frontend/mobile/src/firebaseConfig.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyD9gpgLG8rF9Ix3vuYG_7oRVEnjKZP3CZc",
   authDomain: "com.anonymous.mobile.firebaseapp.com",
-  projectId: "com-anonymous-mobile",
+  projectId: "niteout-6c93f",
   storageBucket: "com-anonymous-mobile.appspot.com",
   appId: "1:575342652161:ios:5af9429a5c937965998900",
 };
@@ -18,5 +18,10 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Disable offline persistence
+// db.disableNetwork()
+//   .then(() => console.log("Firestore network disabled for testing"))
+//   .catch((error) => console.error("Error disabling network:", error));
 
 export { auth, db };
