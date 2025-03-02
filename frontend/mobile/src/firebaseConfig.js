3512eda643583d58872ae4abaf4f77468da4bbe7
ishaanJ91 <jaini@tcd.ie>
2025-03-02 17:57:51 +0000

    added image in publican

--- frontend/mobile/src/firebaseConfig.js
+++ frontend/mobile/src/firebaseConfig.js
@@ -1,27 +1,16 @@
-// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCsXXjAFlESkikhj7aWi3hs7vFlrNj3soQ",
-  // authDomain: "com.anonymous.mobile.firebaseapp.com",
   projectId: "niteout-49dc5",
-  // storageBucket: "com-anonymous-mobile.appspot.com",
+  storageBucket: "niteout-storage-49dc5",
   appId: "1:77171862574:ios:09ee78bf87992d1d2103bf",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Disable offline persistence
-// db.disableNetwork()
-//   .then(() => console.log("Firestore network disabled for testing"))
-//   .catch((error) => console.error("Error disabling network:", error));
 
 export { auth, db };
